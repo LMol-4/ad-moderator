8e11a28dd8bd3ffc5b19db522f2bb7f3f0fba011
Luke <154251365+LMol-4@users.noreply.github.com>
2025-10-04 16:19:13 +0100

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-declare function testAdModerator(): Promise<void>;
-declare function createTestImageDirectory(): void;
-export { testAdModerator, createTestImageDirectory };
-//# sourceMappingURL=index.d.ts.map
